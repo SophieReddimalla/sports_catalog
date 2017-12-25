d7cdc6dc7956d42b7b244f40bb9e6a01f2f7ab1e
Sophie Reddimalla <sophie.reddimalla@gmail.com>
2017-12-25 14:39:51 +0530

    google login added with redirect

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template, flash, redirect, url_for, abort
-from catalog import app, db
+from flask import Flask, render_template, flash, redirect, url_for, abort, jsonify, request, make_response
+from flask import session as login_session
 from sqlalchemy import desc
 
+import json
+import random
+import string
+import httplib2
+import requests
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+
+from catalog import app, db
 from forms import UserForm, CategoryForm, ItemForm
 from catalog.models import User, Category, Item
+
+
+CLIENT_ID = json.loads(
+    open('./catalog/client_secrets.json', 'r').read())['web']['client_id']
+
+
 
 def get_current_user():
     user = User.query.get(1)   # 1-Samson ; 3 - Sophie
@@ -87,8 +103,8 @@ def category_delete(category_id):
                   " and all its items have been deleted successfully!!")
         return redirect(url_for('category_list'))
     else:
-        return render_template('error_not_authorized.html',
-                               message='Category either not created by you or has items not created by you')
+        flash("All that we know either that this category or its items are not created by you hence you cannot delete it!!")
+    return redirect(url_for('category_list'))     
 
 
 @app.route('/category/edit/<int:category_id>', methods=['GET', 'POST'])
@@ -104,19 +120,8 @@ def category_edit(category_id):
             return redirect(url_for('category_list'))
         return render_template('category_edit.html', form=form)
     else:
-        return render_template('error_not_authorized.html',
-                               message='This category is not created by you stupid!!')
-
-
-# ----------------------------------------------------------------------------------------------##
-# ITEMS ELEMENTS:
-# ---------------------------------------------------------------------------------------------##
-
-
-@app.route('/latest_items')
-def latest_item():
-    items = Item.query.order_by(Item.created.desc()).limit(2)
-    return render_template('latest_item.html', items=items)
+        flash("All that we know that either this category or its items are not created by you hence you cannot edit it!!")
+    return redirect(url_for('category_list'))
 
 
 @app.route('/item/view/<int:item_id>')
@@ -143,10 +148,8 @@ def item_create(category_id):
 @app.route('/item/delete_confirm/<int:item_id>')
 def item_delete_confirm(item_id):
     item = Item.query.get(item_id)
-    db.session.delete(item)
-    db.session.commit()
-    return render_template('item_delete_confirm.html')
-
+    return render_template('item_delete_confirm.html', item=item)
+    
 
 @app.route('/item/delete/<int:item_id>')
 def item_delete(item_id):
@@ -156,13 +159,15 @@ def item_delete(item_id):
             db.session.commit()
             return render_template('item_delete.html')
 
-    return render_template('error_not_authorized.html', message=item.title)
+    flash("This item is not been created by you.." )
 
 
 @app.route('/item/edit/<int:item_id>', methods=['GET', 'POST'])
 def item_edit(item_id):
     item = Item.query.get(item_id)
-    if item.user_id == get_current_user():
+    if item.user_id != get_current_user():
+        flash("This item is not been created by you.." )
+    else:    
         form = ItemForm(obj=item)
         if form.validate_on_submit():
             form.populate_obj(item)
@@ -171,11 +176,99 @@ def item_edit(item_id):
             flash(item.title + ' updated successfully')
             return redirect(url_for('category_view', category_id=item.category_id))
         return render_template('item_edit.html', form=form)
-    return render_template('error_not_authorized.html', message=item.title)
 
-# -------------------------------------------------------------------------------------------##
-# LOGIN LOGOUT ELEMENTS:
-# -------------------------------------------------------------------------------------------##
+
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
+
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('catalog/client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!-" +  login_session['username']
+    return output
 
 @app.route('/logout')
 def logout():
